084503be454012f78a8ed3e94de612a08df00037
Mykyla Jaimie Valenzuela <mjquitain5@gmail.com>
2026-01-19 03:29:08 +0800

    feat: Add Auth APIs (register, login, logout)

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        const connectionInstance = await mongoose.connect
+            (`${process.env.MONGODB_URI}`)
         console.log("MongoDB Connected");
     } catch (error) {
         console.error("MongoDB Connection Failed:", error);
