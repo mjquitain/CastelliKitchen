3f56cb30d04a443490e0d0ad314714ed19bcdeea
Claire Monghit <lr.dcmonghit@mmdc.mcl.edu.ph>
2026-01-22 15:47:15 +0800

    Edit Recipe model formatting

--- backend/src/models/recipe.model.js
+++ backend/src/models/recipe.model.js
@@ -2,45 +2,14 @@ import mongoose, { Schema } from "mongoose";
 
 const RecipeSchema = new Schema(
     {
-        name: {
-            type: String,
-            required: true,
-            minLength: 1,
-            maxLength: 100
-        },
-        category: {
-            type: String,
-            enum: ['Beef', 'Chicken', 'Vegetarian', 'Vegan', 'Dessert', 'Lamb', 'Miscellaneous', 'Pasta', 'Seafood', 'Side', 'Pork', 'Breakfast', 'Goat', 'Starter'],
-            required: true,
-            minLength: 1,
-            maxLength: 50
-        },
-        area: {
-            type: String,
-            required: true,
-            minLength: 1,
-            maxLength: 50
-        },
-        ingredients: {
-            type: [String],
-            required: true,
-            // ? ingredients are in long text, separated by comma
-        },
-        instructions: {
-            type: String,
-            required: true,
-            minLength: 10,
-            maxLength: 3000
-        },
-        videolink: {
-            type: String,
-            required: false,
-            // min and max length
-        }
+        name: { type: String, required: true, minLength: 1, maxLength: 100 },
+        category: { type: String, enum: ['Beef', 'Chicken', 'Vegetarian', 'Vegan', 'Dessert', 'Lamb', 'Miscellaneous', 'Pasta', 'Seafood', 'Side', 'Pork', 'Breakfast', 'Goat', 'Starter'], required: true, minLength: 1, maxLength: 50 },
+        area: { type: String, required: true, minLength: 1, maxLength: 50 },
+        ingredients: { type: [String], required: true },
+        instructions: { type: String, required: true, minLength: 10, maxLength: 3000 },
+        videolink: { type: String, required: false }
     },
-    {
-        timestamps: true
-    }
+    { timestamps: true }
 )
 
 export const Recipe = mongoose.model("Recipe", RecipeSchema);
