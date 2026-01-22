9348df5f3bb4feb2d7a1c3055913a51306a8f956
Claire Monghit <lr.dcmonghit@mmdc.mcl.edu.ph>
2026-01-22 17:34:44 +0800

    Connect recipes to app

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,10 @@ const app = express();
 app.use(express.json());
 
 import userRouter from "./routes/user.routes.js";
+import recipeRouter from "./routes/recipe.routes.js";
 
 // Routes
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/recipes", recipeRouter);
 
 export default app;
