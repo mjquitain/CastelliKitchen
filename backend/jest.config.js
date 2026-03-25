3934563ddece7dfc6d6e54102a0d22499c8cc00f
Mykyla Jaimie Valenzuela <mjquitain5@gmail.com>
2026-03-26 01:07:47 +0800

    Add backend integration tests and Jest setup

---
+++ backend/jest.config.js
@@ -0,0 +1,6 @@
+export default {
+    testEnvironment: "node",
+    testMatch: ["**/tests/integration/**/*.test.js"],
+    setupFilesAfterEnv: ["<rootDir>/tests/integration/setup.js"],
+    clearMocks: true,
+};
